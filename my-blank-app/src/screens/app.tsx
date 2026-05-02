7733c43b734b983c3f45b916c3d27ecf3c72a1e9
山本　太陽 <yamamototaiyou.811@gmail.com>
2026-05-02 20:27:01 +0900

    init

---
+++ my-blank-app/src/screens/app.tsx
@@ -0,0 +1,26 @@
+import { registerRootComponent } from 'expo';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+const App = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Open up app.tsx to start</Text>
+            <StatusBar style='auto' />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
+
+// exportを忘れるとホットリロードができないので注意
+export default App;
+
+registerRootComponent(App);

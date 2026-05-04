2cc032bedd43a4104417d99436c87d57146f08f0
山本　太陽 <yamamototaiyou.811@gmail.com>
2026-05-04 20:56:30 +0900

    サーバー作成

---
+++ todo-app-backend/src/app.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/health', (_, res) => {
+    res.status(200).send({ status: 'OK' });
+});
+
+export { app };
